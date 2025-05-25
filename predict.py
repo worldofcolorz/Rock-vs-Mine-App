6c428d46234c2d681f85e65ced736da0a49eecab
ujjwal <pointsdate@gmail.com>
2025-05-25 12:46:38 +0530

    Add predict.py

--- predict.py
+++ predict.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-# predict.py
-
 import numpy as np
 import joblib
 
@@ -23,29 +20,3 @@ def predict_object(input_data):
             return "This object is a Mine"
     except Exception as e:
         return f"⚠️ Error: {str(e)}"
-=======
-# predict.py
-
-import numpy as np
-import joblib
-
-# Load the saved model
-model = joblib.load('model.pkl')
-
-# Predict function
-def predict_object(input_data):
-    try:
-        # Convert input string of numbers into numpy array
-        input_array = np.asarray(input_data, dtype=float).reshape(1, -1)
-
-        # Make prediction
-        prediction = model.predict(input_array)
-
-        # Return readable result
-        if prediction[0] == 'R':
-            return "This object is a Rock"
-        else:
-            return "This object is a Mine"
-    except Exception as e:
-        return f"⚠️ Error: {str(e)}"
->>>>>>> bece6350bae68e91227afc705874adefa681a7af
